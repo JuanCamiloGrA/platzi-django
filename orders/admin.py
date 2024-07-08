c95ed7c1e6fe601c80b0b4df99f74bffb1c4e8a0
JuanCamiloGrisales <grisalesariasjuancamilo@gmail.com>
2024-07-08 17:58:17 -0500

    chore: Add orders app and update project URLs

---
+++ orders/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Order, OrderProduct
+
+# Register your models here.
+
+class OrderProductInlineAdmin(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderProductInlineAdmin]
+    list_display = ['user', 'order_date', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['user__username']
+
+admin.site.register(Order, OrderAdmin)
---
+++ orders/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+# Create your models here.
+class Order(models.Model):
+    '''Model definition for Order.'''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        '''Meta definition for Order.'''
+
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return f'{self.user} - {self.order_date}'
+    
+class OrderProduct(models.Model):
+    '''Model definition for OrderProduct.'''
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        '''Meta definition for OrderProduct.'''
+
+        verbose_name = 'OrderProduct'
+        verbose_name_plural = 'OrderProducts'
+
+    def __str__(self):
+        return f'{self.order} - {self.product} - {self.quantity}'
